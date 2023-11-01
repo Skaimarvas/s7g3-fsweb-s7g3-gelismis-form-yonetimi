4320b61785488fb013d93a6f1ff65f6498484b72
skaimarvas <skaimarvas@gmail.com>
2023-11-01 19:27:05 +0300

    Validation bölümü yapıldı

--- kullanici-katilimi/src/componenets/Form.js
+++ kullanici-katilimi/src/componenets/Form.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
+import * as Yup from "yup";
 
 const emptyMember = {
   name: "",
@@ -11,8 +13,57 @@ const emptyMember = {
 //Boş bir üye objesi oluşturduk
 const FormCard = ({ teammember = emptyMember }) => {
   const [member, setMember] = useState(teammember);
-
   //teammember propsunu empyMember değişkenine atadık.
+  const [fromErrors, setFormErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    termsofservice: "",
+  });
+
+  const [formValid, setFormValid] = useState(true);
+
+  const teamFormSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("İsim alanı boş bırakılamaz!")
+      .min(3, "Ürün ismi 3 karakterden az olamaz."),
+    email: Yup.string()
+      .email("Geçerli bir email adresi olması gerekiyor!")
+      .required("Email adresi yazılması gerekiyor!"),
+    password: Yup.string()
+      .required("Şifre gerekiyor")
+      .min(6, "Şifre 6 karakterden fazla olmalı!"),
+    termsofservice: Yup.boolean().oneOf(
+      [true],
+      "Kullanım şartlarını kabul etmeniz gerekiyor"
+    ),
+  });
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+
+    for (let key in member) {
+      console.log("checkValidationFor(key, member[key] >", key, member[key]);
+      checkValidationFor(key, member[key]);
+    }
+
+    if (formValid) {
+      console.log("FROM SUBMIT EDİLDİ! ", e);
+      axios
+        .post("https://reqres.in/api/users/", member)
+        .then((res) => {
+          if (res.status === 201) {
+            console.log("Üye başarıyla kaydedildi.");
+            console.log("API tarafından dönen veri:", res.data, res.status);
+          } else {
+            console.log("Üye kaydedilirken bir hata oluştu.");
+          }
+        })
+        .catch((err) => {
+          console.error("Üye kaydedilirken bir hata ile karşılaşıldı", err);
+        });
+    }
+  };
 
   const inputChangeHandler = (e) => {
     const { name, value, type, checked } = e.target;
@@ -22,11 +73,36 @@ const FormCard = ({ teammember = emptyMember }) => {
     console.log("Type", type);
     console.log("Checked", checked);
     console.log("Member", member);
+    checkValidationFor(name, type === "checkbox" ? checked : value);
   };
 
-  const formSubmit = (e) => {
-    e.preventDefault();
-    console.log("FROM SUBMIT EDİLDİ! ", e);
+  useEffect(() => {
+    console.error("form error >", fromErrors);
+  }, [fromErrors]);
+  //fromErros'da bir değişiklik olduğunda konsolda hata belirecek
+
+  useEffect(() => {
+    teammember && setMember(teammember);
+  }, [teammember]);
+
+  useEffect(() => {
+    console.log("member >", member);
+    teamFormSchema.isValid(member).then((valid) => setFormValid(valid));
+  }, [member]);
+
+  const checkValidationFor = (field, value) => {
+    Yup.reach(teamFormSchema, field)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({ ...fromErrors, [field]: "" });
+      })
+      .catch((err) => {
+        console.log("HATA!", field, err.errors[0]);
+        setFormErrors((prevFormErrors) => ({
+          ...prevFormErrors,
+          [field]: err.errors[0],
+        }));
+      });
   };
 
   return (
@@ -40,7 +116,11 @@ const FormCard = ({ teammember = emptyMember }) => {
             onChange={inputChangeHandler}
             value={member.name}
             name="name"
+            isInvalid={!!fromErrors.name}
           />
+          <Form.Control.Feedback type="invalid">
+            {fromErrors.name}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label htmlFor="email">Email</Form.Label>
@@ -50,7 +130,11 @@ const FormCard = ({ teammember = emptyMember }) => {
             onChange={inputChangeHandler}
             value={member.email}
             name="email"
+            isInvalid={!!fromErrors.email}
           />
+          <Form.Control.Feedback type="invalid">
+            {fromErrors.email}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label htmlFor="password">Şifre</Form.Label>
@@ -60,7 +144,11 @@ const FormCard = ({ teammember = emptyMember }) => {
             onChange={inputChangeHandler}
             value={member.password}
             name="password"
+            isInvalid={!!fromErrors.password}
           />
+          <Form.Control.Feedback type="invalid">
+            {fromErrors.password}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Check
@@ -68,7 +156,7 @@ const FormCard = ({ teammember = emptyMember }) => {
             type="checkbox"
             label="Kullanım Şartlarını Kabul Ediyorum"
             onChange={inputChangeHandler}
-            checked={member.check}
+            checked={member.termsofservice}
             name="termsofservice"
           />
         </Form.Group>
